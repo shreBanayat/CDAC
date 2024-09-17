ed118bf80c1b63eecafacf85618e10234b330930
shreBanayat <107065555+shreBanayat@users.noreply.github.com>
2024-09-17 23:04:37 +0530

    Add files via upload

--- OOPJ/Assignments/Sandeep Sir's Assignment-6/Main.java
+++ OOPJ/Assignments/Sandeep Sir's Assignment-6/Main.java
@@ -1,36 +1,33 @@
-import java.util.Arrays;
 
 public class Main {
-	
-	 public static int removeDuplicates(int[] arr) {
-	        if (arr.length == 0) {
-	            return 0;
-	        }
-	        Arrays.sort(arr);
-	        int j = 0;
 
-	        for (int i = 1; i < arr.length; i++) {
-	           
-	            if (arr[i] != arr[j]) {
-	                j++;
-	                arr[j] = arr[i]; 
-	            }
-	        }
-
-	        return j + 1; 
-	    }
-
-	public static void main(String[] args) {
-		int arr[] = {20,50,20,30,40,30,50,50,10}; 
-
-		Main m = new Main(); 
-        int length = m.removeDuplicates(arr);  
-        
-        System.out.println("Array elements=");
-		for(int i=0;i<length;i++) {
-			System.out.println(arr[i]);
+	public static int maximumElement(int[] arr) {
+	int maxElemet=arr[0];
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]>=maxElemet) {
+			maxElemet=arr[i];
 		}
 	}
-}
+	return maxElemet;
+	}
+	
+	public static int minimumElement(int[] arr) {
+		int minElement=arr[0];
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<=minElement) {
+				minElement=arr[i];
+			}
+		}
+		return minElement;
+	}
+	
+	public static void main(String[] args) {
+		int[] intArray= {22,51,44,63,32,14};
+		
+		System.out.println("Maximum elemet from the array is="+Main.maximumElement(intArray));
+		System.out.println("Minimum elemet from the array is="+Main.minimumElement(intArray));
+		
 
-  
+	}
+
+}
